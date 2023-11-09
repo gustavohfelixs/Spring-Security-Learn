c7e1a65204354fea15bc1182522748be4e492653
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2023-11-09 17:44:54 +0000

    controllers + RequestMatchesrs

---
+++ section-one/springsection2/src/main/java/com/gfelix/springsection2/EazyBankBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gfelix.springsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBankBackendApplication.class, args);
+	}
+}
---
+++ section-one/springsection2/src/main/java/com/gfelix/springsection2/controller/LoansController.java
@@ -0,0 +1,14 @@
+package com.gfelix.springsection2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+    
+    @GetMapping("/myLoans")
+    public String getLoans() {
+        return "Here are loans informations from DB";
+    }
+
+}
