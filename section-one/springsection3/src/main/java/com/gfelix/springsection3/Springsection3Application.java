83b280bfd5a181a5b0605e7b81487f1470019489
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2023-11-10 02:56:11 +0000

    jdbc connection

--- section-one/springsection3/src/main/java/com/gfelix/springsection3/Springsection3Application.java
+++ section-one/springsection3/src/main/java/com/gfelix/springsection3/Springsection3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.gfelix.springsection3")
+// @ComponentScan("com.gfelix.springsection3")
 public class Springsection3Application {
 
 	public static void main(String[] args) {
--- section-one/springsection3/src/main/java/com/gfelix/springsection3/config/ProjectSecurityConfig.java
+++ section-one/springsection3/src/main/java/com/gfelix/springsection3/config/ProjectSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.gfelix.springsection3.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,18 +28,28 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager  detailsManager () {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("12345")
-            .authorities("admin")
-            .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("12345")
-            .authorities("user")
-            .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // @Bean
+    // public InMemoryUserDetailsManager  detailsManager () {
+    //     UserDetails admin = User.withDefaultPasswordEncoder()
+    //         .username("admin")
+    //         .password("12345")
+    //         .authorities("admin")
+    //         .build();
+    //     UserDetails user = User.withDefaultPasswordEncoder()
+    //         .username("user")
+    //         .password("12345")
+    //         .authorities("user")
+    //         .build();
+    //     return new InMemoryUserDetailsManager(admin, user);
+    // }
     
 }
