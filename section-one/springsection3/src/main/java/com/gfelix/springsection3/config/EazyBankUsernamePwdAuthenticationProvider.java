17bc5dac80a3af12ae9f4d4b558e882c92638eba
Gustavo Félix <99498636+gustavohfelixs@users.noreply.github.com>
2023-11-12 19:26:07 +0000

    new module add

--- section-one/springsection3/src/main/java/com/gfelix/springsection3/config/EazyBankUsernamePwdAuthenticationProvider.java
+++ section-one/springsection3/src/main/java/com/gfelix/springsection3/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
